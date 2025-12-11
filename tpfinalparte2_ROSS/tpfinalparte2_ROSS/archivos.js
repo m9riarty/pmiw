73b0f750c2c2a60cdf5813257c487a9f96856b92
art <120264859+m9riarty@users.noreply.github.com>
2025-12-11 18:08:58 -0300

    Delete tpfinalparte2_ROSS/tpfinalparte2_ROSS directory

--- tpfinalparte2_ROSS/tpfinalparte2_ROSS/archivos.js
+++
@@ -1,9 +0,0 @@
-let fondos;
-let dipper;
-let piedra;
-
-function preload(){
- fondos = loadImage('/data/bosque.png');
- dipper = loadImage('/data/dipper.png');
- piedra = loadImage('/data/piedra.png');
-}
